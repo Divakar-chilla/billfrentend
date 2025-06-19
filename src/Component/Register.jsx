6d6290e369620fa0cf59bd88a3139b764d856d3e
Divakar-chilla <chilladivakar634@gmail.com>
2025-06-19 23:49:16 +0530

    add functionality to the valid password

--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -6,7 +6,11 @@ import { TbLockPassword } from "react-icons/tb";
 import { MdEventRepeat } from "react-icons/md";
 
 
+
 const Register = () => {
+let [repeatpass, setRepeatPass] = useState('');
+let [isChecked, setisChecked] = useState(false);
+
 let [formdata,setformdata]=useState({
   username:"",
   password:"",
@@ -16,34 +20,31 @@ let [formdata,setformdata]=useState({
 let handelchange=e=>{
   let {name,value}=e.target
   setformdata((preval)=>({...preval,[name]:value}))
+    if (name === "password") {
+      setisChecked(repeatpass && repeatpass !== value);
+    }
 }
 let handelsubmit=e=>{
   e.preventDefault()
   console.log(formdata);
+
 }
+let checkRepeatpassword = (e) => {
+  let value = e.target.value;
+  setRepeatPass(value);
+  setisChecked(value !== formdata.password);
+};
 
 
 
 
 
-
-  // let [password,setpassword]=useState("");
-  // let [retypepassword,setretypepassword]=useState("")
-  // hadelchangepassword=(e)=>{
-  //   setpassword(e.target.value)
-  //   if(retypepassword!==''&& e.target.value !==password){
-  //     setPasswordMatchError(true)
-  //   }else{
-
-  //   }
-
-  // }
 
   return (
     <div className='bg-[#efefef] size-full flex items-center justify-center'>
       <form className='w-1/2 bg-white h-[90%]  flex items-center justify-center flex-col gap-4 px-[80px] py-40 shadow-md rounded-3xl max-sm:w-[90%] ' onSubmit={handelsubmit}>
         <div className='w-full flex justify-center items-center'>
-          <h1 className='font-extrabold max-md '>Registation form</h1>
+          <h1 className='font-extrabold max-md '>Registration Form</h1>
         </div>
         <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
           <input type="text"  name='name' placeholder='Entername'className='w-full outline-none px-4 h-10'onChange={handelchange} />
@@ -58,11 +59,16 @@ let handelsubmit=e=>{
              <span><MdAttachEmail /></span>
         </div>
         <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="password" placeholder='Enterpassword' name='password'className='w-full outline-none px-4 h-10' onChange={handelchange}/>
+          <input type="password" placeholder='Enterpassword' name='password'className='w-full outline-none px-4 h-10'  onChange={handelchange}/>
              <span><TbLockPassword /></span>
         </div >
            <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="password" placeholder='Re-type-password' className='w-full outline-none px-4 h-10' />
+            <input
+    type="password"
+    placeholder='Re-type Password'
+      className={`w-full outline-none px-4 h-10 ${isChecked ? 'border-red-500 bg-red-100' : ''}`}
+      onChange={checkRepeatpassword}
+  />
              <span><MdEventRepeat /></span>
         </div>
         <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm bg-black'>
